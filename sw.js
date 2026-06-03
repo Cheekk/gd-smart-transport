d8ecf80eb155c024e475c77e2122631946e8140b
Cheekk <734391877@qq.com>
2026-06-03 15:26:04 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE = 'gdits-2026-v1'
+const URLS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './data/enterprises.json'
+]
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(URLS).catch(() => {}))
+  )
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => 
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  )
+  self.clients.claim()
+})
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      const fetched = fetch(e.request).then(resp => {
+        if (resp && resp.status === 200) {
+          const clone = resp.clone()
+          caches.open(CACHE).then(cache => cache.put(e.request, clone))
+        }
+        return resp
+      }).catch(() => cached)
+      return cached || fetched
+    })
+  )
+})
